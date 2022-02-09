41561e4da4aac3f8d3765cafb3afb55979ae9fea
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-02-09 20:21:37 -0300

    if a letter is wrong, it is deleted

--- puzzle.py
+++ puzzle.py
@@ -1,7 +1,7 @@
 import random
 from puzzle_2 import main_2
 #TODO: Add a check to see if the player got the word right
-#TODO: Replace some of the wording in the code so it says what our instructor is asking us
+
 class Word:
     def __init__(self):
         fileObject = open("list_words.txt", "r")
@@ -19,8 +19,8 @@ class Player:
         self._puzzle = Puzzle()
         self._guess = ""
         self._guesses = []
-        self._guesses_left = 15
-        self._guesses_allowed = 15
+        self._guesses_left = 20
+        self._guesses_allowed = 20
     def guess_word(self):
         print("The number of letters are: ", len(self._puzzle._word))
         print("The first letter is: ", self._puzzle._word[0], "\nAnd the last one is: ",self._puzzle._word[-1])
@@ -31,13 +31,14 @@ class Player:
             print("Correct!")
         else:
             print("Incorrect!")
+            poping = self._guesses.pop()
             self._guesses_left -= 1
     def display_word(self):
         print("The word is: ", self._puzzle._word)
     def display_guesses(self):
-        print("Your guesses: ", self._guesses)
+        print("Lines in your parachute: ", self._guesses)
     def display_guesses_left(self):
-        print("Guesses left: ", self._guesses_left)
+        print("Lines in your parachute left: ", self._guesses_left)
     def play_game(self):
         while self._guesses_left > 0:
             self.guess_word()
@@ -71,7 +72,7 @@ choice = input("\nEnter 1 or 2: ")
 if choice == "1":
     print("You are in room 1")
     print("You have over four hundred thousand words to guess.")
-    print("You have 15 guesses.")
+    print("You have 20 lines in your parachute.")
     print("You can only guess one letter at a time.")
     print("Good luck!")
     main()
@@ -79,7 +80,7 @@ if choice == "1":
 elif choice == "2":
     print("You are in room 2")
     print("You have ten words to guess.")
-    print("You have 15 guesses.")
+    print("You have 10 lines in your parachute.")
     print("You can only guess one letter at a time.")
     print("Good luck!")
     main_2()
--- puzzle_2.py
+++ puzzle_2.py
@@ -19,35 +19,32 @@ class Parachuter:
         self._puzzle = Words()
         self._guess = ""
         self._guesses = []
-        self._guesses_left = 15
-        self._guesses_allowed = 15
+        self._guesses_left = 10
+        self._guesses_allowed = 10
     def guess_word(self):
         print("The number of letters are: ", len(self._puzzle._word))
         print("The first letter is: ", self._puzzle._word[0])
         self._guess = input("\nEnter a letter: ")
         self._guesses.append(self._guess)
     def check_guess(self):
+        
         if self._guess in self._puzzle._word:
             print("\nCorrect!")
         else:
             print("\nIncorrect!")
+            poping = self._guesses.pop()
             self._guesses_left -= 1
-    #This is not working yet, so if you need to use it, you will need to fix it or comment it out
-    def compare_guesses(self):
-        self.correct= []
-        for i in self._guesses:
-            right_word = self.correct.append(i)
-            return self.correct
-    print(f"trololololololo {compare_guesses()}")
     def display_word(self):
         print("The word is: ", self._puzzle._word)
     def display_guesses(self):
         print("Your guesses: ", self._guesses)
     def display_guesses_left(self):
         print("Guesses left: ", self._guesses_left)
+    
     def play_game(self):
+        list_new = self._guesses
+        
         while self._guesses_left > 0:
-            self.compare_guesses()
             self.guess_word()
             self.check_guess()
             self.display_guesses()
@@ -56,16 +53,14 @@ class Parachuter:
                 print("\nYou lost!")
                 print(self.display_word())
                 break
-            if self._guess == self._puzzle._word:
+            
+            if self._puzzle._word in list_new:
                 print("\nYou won!")
                 print(self.display_word())
 #TODO: Add a check to see if the player got the word right
-#This part is not working either
-                if self.compare_guesses() in self._puzzle._word:
-                    print("You got it!")
-                    print(self.display_word())
-                    break
+
                 break
+
 def main_2 ():
     obj1 = Parachuter()
     obj1.play_game()
