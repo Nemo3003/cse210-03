222e98ac6357aedf195eada1d2479c7213730a3c
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-02-18 08:24:15 -0300

    fixed

--- puzzle.py
+++ puzzle.py
@@ -47,8 +47,10 @@ class Player:
             self.display_incorrect()
             
             if self._guesses_left == 0:
-                print("\nYou lost!")
+                print("\nYou lost!") 
+                print("I'm so sorry, but you lost the game. Be thankful that this is just a game, otherwise you could have lost your life :( ")
                 print(self.display_word())
+                
                 restart = input("Do want to play Again?(y/n): ")
                 if restart.lower() == "y":
                     main()
@@ -57,6 +59,7 @@ class Player:
             new_trans = ''.join(map(str, self._guesses))
             if new_trans == self._puzzle._word:
                 print("You won!! Congrats!!")
+                print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                 restart = input("Do want to play Again?(y/n): ")
                 if restart.lower() == "y":
                     main()
@@ -70,76 +73,91 @@ class Player:
                     case 2:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 3:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 4:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 5:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses and self._puzzle._word[4] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 6:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses and self._puzzle._word[4] in self._guesses and self._puzzle._word[5] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 7:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses and self._puzzle._word[4] in self._guesses and self._puzzle._word[5] in self._guesses and self._puzzle._word[6] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 8:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses and self._puzzle._word[4] in self._guesses and self._puzzle._word[5] in self._guesses and self._puzzle._word[6] in self._guesses and self._puzzle._word[7] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 9:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses and self._puzzle._word[4] in self._guesses and self._puzzle._word[5] in self._guesses and self._puzzle._word[6] in self._guesses and self._puzzle._word[7] in self._guesses and self._puzzle._word[8] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 10:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses and self._puzzle._word[4] in self._guesses and self._puzzle._word[5] in self._guesses and self._puzzle._word[6] in self._guesses and self._puzzle._word[7] in self._guesses and self._puzzle._word[8] in self._guesses and self._puzzle._word[9] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 11:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses and self._puzzle._word[4] in self._guesses and self._puzzle._word[5] in self._guesses and self._puzzle._word[6] in self._guesses and self._puzzle._word[7] in self._guesses and self._puzzle._word[8] in self._guesses and self._puzzle._word[9] in self._guesses and self._puzzle._word[10] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 12:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses and self._puzzle._word[4] in self._guesses and self._puzzle._word[5] in self._guesses and self._puzzle._word[6] in self._guesses and self._puzzle._word[7] in self._guesses and self._puzzle._word[8] in self._guesses and self._puzzle._word[9] in self._guesses and self._puzzle._word[10] in self._guesses and self._puzzle._word[11] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 13:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses and self._puzzle._word[4] in self._guesses and self._puzzle._word[5] in self._guesses and self._puzzle._word[6] in self._guesses and self._puzzle._word[7] in self._guesses and self._puzzle._word[8] in self._guesses and self._puzzle._word[9] in self._guesses and self._puzzle._word[10] in self._guesses and self._puzzle._word[11] in self._guesses and self._puzzle._word[12] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 14:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses and self._puzzle._word[4] in self._guesses and self._puzzle._word[5] in self._guesses and self._puzzle._word[6] in self._guesses and self._puzzle._word[7] in self._guesses and self._puzzle._word[8] in self._guesses and self._puzzle._word[9] in self._guesses and self._puzzle._word[10] in self._guesses and self._puzzle._word[11] in self._guesses and self._puzzle._word[12] in self._guesses and self._puzzle._word[13] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 15:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses and self._puzzle._word[4] in self._guesses and self._puzzle._word[5] in self._guesses and self._puzzle._word[6] in self._guesses and self._puzzle._word[7] in self._guesses and self._puzzle._word[8] in self._guesses and self._puzzle._word[9] in self._guesses and self._puzzle._word[10] in self._guesses and self._puzzle._word[11] in self._guesses and self._puzzle._word[12] in self._guesses and self._puzzle._word[13] in self._guesses and self._puzzle._word[14] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
                     case 16:
                         if self._puzzle._word[0] in self._guesses and self._puzzle._word[1] in self._guesses and self._puzzle._word[2] in self._guesses and self._puzzle._word[3] in self._guesses and self._puzzle._word[4] in self._guesses and self._puzzle._word[5] in self._guesses and self._puzzle._word[6] in self._guesses and self._puzzle._word[7] in self._guesses and self._puzzle._word[8] in self._guesses and self._puzzle._word[9] in self._guesses and self._puzzle._word[10] in self._guesses and self._puzzle._word[11] in self._guesses and self._puzzle._word[12] in self._guesses and self._puzzle._word[13] in self._guesses and self._puzzle._word[14] in self._guesses and self._puzzle._word[15] in self._guesses:
                             print("\nWell done! you finally made it!!")
+                            print("To celebrate your victory I would invite you a beer but my creator does not drink beer, I am a computer program and I guess you don't drink beer either. So I will just say, well done!")
                             print("The word was",  self._puzzle._word)
                             break
 
